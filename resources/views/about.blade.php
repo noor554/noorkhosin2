a5c8d776c9b28af41cf979f72ba8c9efffb5217f
Noor Khosin <noorkhosin0202@gmail.com>
2020-11-20 10:20:23 +0900

    commit pertama

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.main')
+
+@extends('title',About')
+   
+   <div class="container">
+   <div class="row">
+    <div class="col-10">
+    <h1> class="mt-3">Hello, pak Fridolin</h1>
+     </div>
+  </div>
+</div>
+@endsection
+
+    
